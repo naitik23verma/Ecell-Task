de736e8fb877f761286c3a2d624e46e32da5bc71
Naitik Verma <naitikv2311@gmail.com>
2025-03-28 16:46:58 +0530

    again change

--- Ecell/Ecell/script.js
+++ Ecell/Ecell/script.js
@@ -1,11 +1,16 @@
+
+
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const menu = document.querySelector(".nav_items");
+
 menuBtn.addEventListener("click", () => {
     menu.style.display = 'block';
     menuBtn.style.display = 'none';
     closeBtn.style.display = 'inline-block';
 })
+
+
 const closeNavMenu = () => {
     menu.style.display = 'none';
     menuBtn.style.display = 'inline-block';
@@ -15,12 +20,15 @@ const closeNavMenu = () => {
 closeBtn.addEventListener("click", () => {
     closeNavMenu();
 })
+
+
+
 const faqs = document.querySelectorAll('.faq');
 
 faqs.forEach(faq => {
     faq.addEventListener('click', () => {
         faq.classList.toggle('open');
-
+        // change icon
         const icon = faq.querySelector('.faq_icon i');
         if(icon.className === 'uil uil-plus') {
             icon.className = 'uil uil-minus';
@@ -29,9 +37,15 @@ faqs.forEach(faq => {
         }
     })
 })
+
+
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
 })
+
+
+
+
 const navItems = document.querySelectorAll('.nav_items li a');
 if(window.innerWidth < 1024) {
     navItems.forEach(item => {
